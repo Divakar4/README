217537476f6800312fa40dccd0b71f8c24a2121e
Divakar <155337699+Divakar4@users.noreply.github.com>
2024-02-21 13:40:00 +0530

    created sum.c

--- sum.c
+++ sum.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
+#include<stdlib.h>
 int main()
 {
 int *p;
 p=malloc(4*sizeof(int));
 for(int i=0;i<4;i++){
-  scanf("%d",p[i]);
+  scanf("%d",&p[i]);
 }
 int sum=0;
 for(int i=0;i<4;i++){
